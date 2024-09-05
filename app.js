d29eedebd93aa7c617a8e7f413cdc04bb286354a
pedroporo <pedroguillferri9@gmail.com>
2024-09-05 10:59:53 +0200

    vault backup: 2024-09-05 10:59:53

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-const searchHandler = require("./netlify/functions/search/search.js").handler;
+//const searchHandler = require("./netlify/functions/search/search.js").handler;
 
 app.get("/api/search", async (req, res) => {
   //Mock the netlify event
   let event = { queryStringParameters: req.query };
 
-  let response = await searchHandler(event);
-  res.status(response.statusCode).send(response.body);
+  //let response = await searchHandler(event);
+  // res.status(response.statusCode).send(response.body);
 });
 
 app.use(express.static("dist"));
