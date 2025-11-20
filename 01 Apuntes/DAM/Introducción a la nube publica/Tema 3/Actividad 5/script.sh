0bc5112289f1aa7a87a0d7bd4ee459bc4143b83b
pedroporo <pedroguillferri9@gmail.com>
2025-11-20 17:28:47 +0100

    vault backup: 2025-11-20 17:28:47

---
+++ 01 Apuntes/DAM/Introducción a la nube publica/Tema 3/Actividad 5/script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+INSTANCE_COUNT=4
+INSTANCE_TYPE="t2.micro"
+AMI_ID="ami-0c7217cdde317cfec"
+KEY_NAME="tu-clave-ssh"  # Reemplaza con tu clave SSH
+SECURITY_GROUP="sg-12345678"  # Reemplaza con tu grupo de seguridad
+REGION="us-east-1"  
+
+
+create_instance() {
+  local instance_name=$1
+  local user_data=$2
+
+  aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count 1 \
+    --instance-type $INSTANCE_TYPE \
+    --key-name $KEY_NAME \
+    --security-group-ids $SECURITY_GROUP \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" \
+    --user-data "$user_data" \
+    --region $REGION
+}
+
+
+for i in {1..4}; do
+  instance_name="Webserver-$i"
+  user_data="#!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo '<html><body><h1>Webserver $i de Pedro Guill ferri en la màquina $i</h1></body></html>' > /var/www/html/index.html
+  "
+  create_instance "$instance_name" "$user_data"
+done
